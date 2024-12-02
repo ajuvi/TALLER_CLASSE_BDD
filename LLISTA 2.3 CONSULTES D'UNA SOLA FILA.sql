7a53910ac0ab565289c73622c259001c3e6b8c5f
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-12-02 07:45:19 +0100

    artur

--- LLISTA 2.3 CONSULTES D'UNA SOLA FILA.sql
+++ LLISTA 2.3 CONSULTES D'UNA SOLA FILA.sql
@@ -63,3 +63,78 @@ FROM employees;
 
 SELECT last_name, hire_date, WEEKDAY(hire_date) AS dia
 FROM employees;
+
+--EX15
+--------
+
+SELECT last_name, IFNULL(commission_pct,'No commission')
+FROM employees;
+
+SELECT last_name, NVL(commission_pct,'No commission')
+FROM employees;
+
+SELECT last_name, NVL2(commission_pct,commission_pct,'No commission')
+FROM employees;
+
+SELECT last_name, COALESCE(commission_pct, 'No commission')
+FROM employees;
+
+
+-- Exemple de COALESCE
+
+SELECT COALESCE(NULL, 2*NULL, 1+NULL, 99, NULL, 80) AS result
+FROM DUAL;
+
+SELECT COALESCE('NULL', NULL, 99 ) AS result
+FROM DUAL;
+
+SELECT COALESCE(1,2,3,4,5 ) AS result
+FROM DUAL;
+
+--EX16
+--------
+
+SELECT employee_id, last_name, job_id, DECODE_ORACLE(job_id,'AD_PRES','A', 'ST_MAN', 'B', 'IT_PROG', 'C', 'SA_REP', 'D', 'ST_CLERK', 'I', 'O') AS ID
+FROM employees;
+
+SELECT  employee_id, 
+        last_name, 
+        job_id, 
+        DECODE_ORACLE(job_id,
+                      'AD_PRES','A', 
+                      'ST_MAN', 'B', 
+                      'IT_PROG', 'C', 
+                      'SA_REP', 'D', 
+                      'ST_CLERK', 'I', 
+                      'O') AS qualificacio
+FROM employees;
+
+--EX17
+--------
+
+SELECT  employee_id, 
+        last_name, 
+        job_id, 
+        CASE
+            WHEN job_id='AD_PRES' THEN 'A'
+            WHEN job_id='ST_MAN' THEN 'B'
+            WHEN job_id='IT_PROG' THEN 'C'                        
+            WHEN job_id='SA_REP' THEN 'D'
+            WHEN job_id='ST_CLERK' THEN 'I'
+            ELSE 'O'
+        END AS qualificacio
+FROM employees;         
+
+-- Exemple de CASE WHEN similar al DECODE_ORACLE
+SELECT  employee_id, 
+        last_name, 
+        job_id, 
+        CASE job_id
+            WHEN 'AD_PRES' THEN 'A'
+            WHEN 'ST_MAN' THEN 'B'
+            WHEN 'IT_PROG' THEN 'C'                        
+            WHEN 'SA_REP' THEN 'D'
+            WHEN 'ST_CLERK' THEN 'I'
+            ELSE 'O'
+        END AS qualificacio
+FROM employees;  
