ff631bfe4aaf9313e6ff168d4f9e60c6a8f9f13e
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-04-03 10:51:58 +0200

    Update LLISTA A4.3 Procediments i funcions.sql

--- LLISTA A4.3 Procediments i funcions.sql
+++ LLISTA A4.3 Procediments i funcions.sql
@@ -21,3 +21,35 @@ end;
 
 call generarMessages(10);
 select * from messages;
+
+
+create or replace function estadistiquesEmpleats()
+returns varchar(255)
+begin 
+	declare v_result varchar(255);
+	declare v_totalEmpleats int;
+	declare v_totalEmpleatsCommissio int;
+	declare v_empleats2000 int;
+	declare v_anysTreballats int;
+	declare v_totalEmpleats10anys int;
+
+	select count(*), count(commission_pct)
+	into   v_totalEmpleats, v_totalEmpleatsCommissio
+	from employees;
+
+	select count(*) into v_empleats2000
+	from employees 
+	where year(hire_date) = 2000;
+
+	set v_result = concat_ws(';',v_totalEmpleats,v_totalEmpleatsCommissio,v_empleats2000);
+
+	return v_result;
+		
+end;
+
+select estadistiquesEmpleats()
+from dual;
+
+
+
+
